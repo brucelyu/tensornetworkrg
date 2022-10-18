86ad9669d583c93338df2b8fb9be142b48e07e7a
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-10-19 01:12:38 +0900

    start 3d hotrg implementation

---
+++ coarse_grain_3d/hotrg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : hotrg.py
+# Author            : Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
+# Date              : 15.10.2022
+# Last Modified Date: 15.10.2022
+# Last Modified By  : Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
+
+import numpy as np
+from ncon import ncon
+# TODO
+
