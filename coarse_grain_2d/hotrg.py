a1734f585b0e470a6278292b2390fcdd63e32128
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2022-11-25 23:03:33 +0900

    reflection-symmetric 2d HOTRG implemented

--- coarse_grain_2d/hotrg.py
+++ coarse_grain_2d/hotrg.py
@@ -39,6 +39,148 @@ def opt_v(Ain, chi, dtol=1e-10):
     return v, SPerr
 
 
+# --- The following three functions are for                     ---|
+# --- the reflection-symmetric implmentation of the 2d HOTRG --    |
+def opt_vDown(Ain, chi, dtol=1e-10):
+    """determine the isometric tensor for a 2d reflection-symmetric HOTRG.
+    Basically the same as `opt_v`;
+    the difference is the convention
+    of gauge in the design
+
+    Args:
+        Ain (TensorCommon): 4-leg tensor A[i, j, k, l]
+        chi (int): squeezed bond dimension
+
+    Kwargs:
+        dtol (float): truncate eigenvectors with
+            eigenvalues smaller than dtol
+
+    Returns:
+        vDown (TensorCommon): 3-leg isometric tensor
+
+    """
+    B = Ain.conjugate()
+    env_v = ncon([Ain, B, Ain.conjugate(), B.conjugate()],
+                 [[-1, 1, 2, 5], [-2, 3, 4, 5],
+                  [-3, 1, 2, 6], [-4, 3, 4, 6]
+                  ])
+    (d,
+     v,
+     SPerr
+     ) = env_v.eig([0, 1], [2, 3],
+                   hermitian=True,
+                   chis=[i+1 for i in range(chi)],
+                   trunc_err_func=trunc_err_func,
+                   eps=dtol, return_rel_err=True)
+    return v, SPerr
+
+
+def block2tensors(A, v, vin):
+    """block A and its reflection in vertical direction a la HOTRG
+
+    Args:
+        A (TensorCommon): 4-leg tensor
+        v (TensorCommon): outer isometry (left)
+        vin (TensorCommon): inner isometry (right)
+
+    Returns:
+        Aout (TensorCommon): 4-leg coarser tensor
+    """
+    Aout = ncon([A, A.conj(), v.conj(), vin],
+                [[1, -2, 3, 4], [5, -4, 2, 4],
+                 [1, 5, -1], [3, 2, -3]])
+    return Aout
+
+
+def blockPlaq(Ain, v, w, vin):
+    """block a 2-by-2 plaquette made of A and its reflection
+    using isometric tensors `v, w, vin` a la HOTRG
+
+    Args:
+        Ain (TensorCommon): 4-leg tensor A[i, j, k, l]
+        v (TensorCommon): 3-leg isometric tensor
+        w (TensorCommon): 3-leg isometric tensor
+        vin (TensorCommon): 3-leg isoemtric tensor
+
+    Returns:
+        Aout (TensorCommon): 4-leg coarser tensor
+
+    """
+    # Block vertically
+    Avc = block2tensors(Ain, v, vin)
+    # Block horizontally
+    Avcreflv = Avc.conj().transpose([2, 1, 0, 3])
+    Aout = block2tensors(Avcreflv.transpose([1, 2, 3, 0]),
+                         w, w)
+    # rotate back
+    Aout = Aout.transpose([3, 0, 1, 2])
+    return Aout
+
+
+def reflSymHOTRG(Ain, chi, dtol=1e-10, horiSym=True):
+    """coarse graining of 2D HOTRG that preserves reflection symmetry
+    Block the following plaquette
+        |     |
+     ---A----Areflv*--
+        |     |
+        |     |
+ --Areflh*----Areflhv--
+        |     |
+    For the blocking part, we
+    1) first block two tensors in the vertical direction, and then
+    2) blocking the resultant tensor in horizontal direction.
+    However, notice both two outer isometric tensors
+    are determined using initial tensor as environment.
+
+    Args:
+        Ain (TensorCommon): 4-leg tensor A[i, j, k, l]
+        chi (int): bond dimension
+        dtol (float): truncate eigenvectors with
+            eigvenvalues smaller than dtol
+        horiSym (boolean): whether does `Ain` have
+            horizontal-reflection symmetry
+
+    Returns:
+        Aout (TensorCommon): 4-leg coarser tensor
+        v (TensorCommon): outer isometry for blocking
+            two tensors vertically
+        vin (TensorCommon): inner isometry for blocking
+            two tensors vertically
+        w (TensorCommon): outer isometry for blocking
+            two tensors horizontally
+    """
+    # determine one outer isometry
+    v, errv = opt_vDown(Ain, chi, dtol)
+    # determine the other outer isometry
+    Areflvstar = Ain.conj().transpose([2, 1, 0, 3])
+    # input the rotated-legs-by-90-degrees tensor
+    w, errw = opt_vDown(Areflvstar.transpose([1, 2, 3, 0]),
+                        chi, dtol)
+    # determine the inner isometry
+    if horiSym:
+        # vin is exactly the same as v
+        vin = v * 1.0
+        errvin = errv
+    else:
+        # determine vin by input Areflv*
+        (vin,
+         errvin
+         ) = opt_vDown(Areflvstar, chi, dtol)
+    # finally we block Ain and v, vin, w and
+    # get the coarser tensor
+    Aout = blockPlaq(Ain, v, w, vin)
+    SPerrList = [errv, errw, errvin]
+    return Aout, v, w, vin, SPerrList
+
+# --- Here is the end of the                                    ---|
+# --- the reflection-symmetric implmentation of the 2d HOTRG --- __|
+
+# --------------------------------------------------------------------- #
+# --------------------------------------------------------------------- #
+
+# --- The following three functions are for                       ---|
+# --- the rotationally-symmetric implmentation of the 2d HOTRG --    |
+
 def block4tensor(Ain, v):
     """
     Block 4 tensors to get a coarser tensor
@@ -88,6 +230,9 @@ def getAuvu(AList, vList, rgstep=5):
     vu = ncon([vcur, gauge.conjugate()],
               [[-1, 2, -3], [2, -2]])
     return Au, vu
+
+# --- Here is the end of the                                   ---   |
+# --- the rotationally-symmetric implmentation of the 2d HOTRG --- __|
 
 
 def trunc_err_func(eigv, chi):
