ab9b6cd5586c3585c62b39cc0b349c70f9e84ea9
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2023-07-18 23:21:25 +0900

    start the 3d block-tensor RG implementation

---
+++ coarse_grain_3d/block_tensor.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : block_tensor.py
+# Author            : Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
+# Date              : 14.07.2023
+# Last Modified Date: 14.07.2023
+# Last Modified By  : Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
+"""
+A HOTRG-like block-tensor coarse graining method
+- It is adapted from the usual 3D HOTRG to
+    suit our entanglement-filtering process
+- It is more general than the usual HOTRG. By adjust the parameter, this method
+    interpolates between usual HOTRG and the full block-tensor transformation,
+    which applies 4-to-1 isometric tensors to 6 faces
+    of a cube of 8 copies of input tensor.
+What's more, the reflection symmetry is exactly imposed here.
+"""
+from .hotrg import zCollapseXproj
+from ncon import ncon
+
+
+def zfind1p(A, chi, which="pmx", cg_eps=1e-8):
+    # 0. For leg permutation
+    # 0.1 For leg permutation in z direction
+    permzleg = [0, 1, 2, 3, 5, 4]
+    # 0.2 For leg permutation x, x', y, y' <---> y, y', x, x'
+    permxy = [2, 3, 0, 1, 4, 5]
+    # 0.3 For leg permutation in x direction
+    permxleg = [1, 0, 2, 3, 4, 5]
+
+    if which == "pmx":
+        Arela = A * 1.0
+    elif which == "pmy":
+        # rotate in xy plane
+        Arela = A.transpose(permxy)
+    elif which == "pix":
+        # reflect x legs + conjugate
+        Arela = A.transpose(permxleg).conj()
+    elif which == "piy":
+        # rotate + reflect + conjugate
+        Arela = A.transpose(permxy).transpose(permxleg).conj()
+    else:
+        raise ValueError("`which` value not correct!")
+
+    (
+        p, err, d
+    ) = zCollapseXproj(
+        Arela, Arela.transpose(permzleg).conj(),
+        chi)
+    return p, err, d
+
+
+def zfindp(A, chiM, chiI, cg_eps=1e-8):
+    """
+    Determine all 4 projectors for coarse graining
+    in z direction
+    """
+    # I. For two intermediate outer projectors
+    # I.1 pmx
+    (
+        pmx, errmx, dmx
+    ) = zfind1p(A, chiM, which="pmx", cg_eps=cg_eps)
+    # I.2 pmy
+    (
+        pmy, errmy, dmy
+    ) = zfind1p(A, chiM, which="pmy", cg_eps=cg_eps)
+
+    # II. For two inner projectors
+    # II.1 pix
+    (
+        pix, errix, dix
+    ) = zfind1p(A, chiI, which="pix", cg_eps=cg_eps)
+    # II.2 piy
+    (
+        piy, erriy, diy
+    ) = zfind1p(A, chiI, which="piy", cg_eps=cg_eps)
+
+    # group projectors
+    zprojs = [pmx, pix, pmy, piy]
+    zerrs = [errmx, errix, errmy, erriy]
+    zds = [dmx, dix, dmy, diy]
+    return zprojs, zerrs, zds
+
+
+def yfindp(Az, chi, chiM, chiII, cg_eps=1e-8):
+    """
+    Projectors for y-direction coarse graining
+    after z-direction one.
+    """
+    # rotation xyz --> zxy
+    Ar = Az.transpose([4, 5, 0, 1, 2, 3])
+    # I. pmz
+    (
+        pmz, errmz, dmz
+    ) = zfind1p(Ar, chiM, which="pmx", cg_eps=cg_eps)
+    # II. pox
+    (
+        pox, errox, dox
+    ) = zfind1p(Ar, chi, which="pmy", cg_eps=cg_eps)
+    # III. piix
+    (
+        piix, erriix, diix
+    ) = zfind1p(Ar, chiII, which="piy", cg_eps=cg_eps)
+
+    # group projectors
+    yprojs = [pmz, pox, piix]
+    yerrs = [errmz, errox, erriix]
+    yds = [dmz, dox, diix]
+    return yprojs, yerrs, yds
+
+
+def zblock(A, pxc, pyc, px, py, comm=None):
+    # contraction for coarse graining process
+    # This version has computation cost O(chi^11) and
+    # memory cost O(chi^8). The memory cost can be reduced to O(chi^6).
+    if comm is None:
+        Aout = ncon([A, A.conj(), pxc, pyc, px, py],
+                    [[1, 2, 6, 7, -5, 5], [8, 9, 3, 4, -6, 5],
+                     [1, 8, -1], [6, 3, -3], [2, 9, -2], [7, 4, -4]
+                     ])
+    return Aout
+
+
+# Useful for algorithm development
+def moErrs(A, chi, chiM, chiI, chiII):
+    """
+    Various RG errors if we coarse grain A
+    """
+    # determine z projectors
+    zpjs, zerr = zfindp(A, chiM, chiI)[:2]
+    pmx, pix, pmy, piy = zpjs
+    # contract z direction
+    Az = zblock(A, pmx.conj(), pmy.conj(), pix, piy)
+    # determine y projectors
+    ypjs, yerr = yfindp(Az, chi, chiM, chiII)[:2]
+    errmx, errix, errmy, erriy = zerr
+    errmz, errox, erriix = yerr
+    return errmx, errmy, errmz, errox, errix, erriy, erriix
