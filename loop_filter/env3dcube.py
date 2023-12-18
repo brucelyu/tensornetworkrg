1d1271c516c9f6e1a71cc3d544dd6724fcede6b0
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2023-12-18 16:42:10 +0900

    wrap up functions to construction P and gamma from doubleA and sy tensors

--- loop_filter/env3dcube.py
+++ loop_filter/env3dcube.py
@@ -223,3 +223,64 @@ def octu2gm(octuA, sy):
     Gamma = ncon([Gamma0, sy, sy.conj()],
                  [[-1, 1, -3, 2], [1, -2], [2, -4]])
     return Gamma
+
+
+# IV. Combine the above functions to construct P and γ
+def dbA2P(dbAp, sy):
+    """Build P from doubleA tensor and sy matrix
+    This is simple a combination of functions in Parts I--III
+
+    Args:
+        dbAp (TensorCommon): 6-leg tensor
+        sy (TensorCommon): 2-leg tensor
+
+    Returns:
+        P (TensorCommon): 2-leg tensor
+
+    """
+    # Cost is χ^4 χs^3
+    dbApy = syOn1Leg(dbAp, sy)
+    # Cost is χ^6 χs^4
+    # For chi=[6,6], chis=[3,3], it takes about 0.4 seconds (CPU times)
+    quadrAp = contrx(dbAp, dbApy.conj())
+    # Cost is χ^6 χs^4 (it is faster than absorbing sy into `quadrAp`)
+    # For chi=[6,6], chis=[3,3], it takes about 0.2 seconds (CPU times)
+    quadrApy = contrx(dbApy, dbApy.conj())
+    # Cost is χ^7 χs^5 <-- bottleneck of the computational cost
+    # For chi=[6,6], chis=[3,3], it takes about 7 seconds (CPU times)
+    # and 1.2 seconds of wall time using 8 threads
+    octuAp = contrz(quadrAp, quadrApy.conj())
+    # Cost is χ^6 χs^3 (it is faster than absorbing sy into `quadrAp`)
+    # For chi=[6,6], chis=[3,3], it takes about 0.4 seconds (CPU times)
+    Psy = octu2P(octuAp, sy)
+    return Psy
+
+
+def dbA2gm(dbAgm, sy):
+    """Build γ from doubleA tensor and sy matrix
+    This is simple a combination of functions in Parts I--III
+
+    Args:
+        dbAgm (TensorCommon): 6-leg tensor
+        sy (TensorCommon): 2-leg tensor
+
+    Returns:
+        Gamma (TensorCommon): 4-leg tensor
+
+    """
+    # Cost is χ^2 χs^5
+    dbAgmy = syOn2Leg(dbAgm, sy)
+    # Cost is χ^2 χs^8
+    # For chi=[6,6], chis=[3,3], it takes about 0.04 seconds (CPU times)
+    quadrAgm = contrx(dbAgm, dbAgmy.conj())
+    # Cost is χ^0 χs^10 (it is faster than absorbing sy into `quadrAgm`)
+    # For chi=[6,6], chis=[3,3], it takes about 0.03 seconds (CPU times)
+    quadrAgmy = contrx(dbAgmy, dbAgmy.conj())
+    # Cost is χ^2 χs^10 <-- bottleneck in this function
+    # For chi=[6,6], chis=[3,3], it takes about 0.37 seconds (CPU times)
+    # and 0.08 seconds of wall time using 8 threads
+    octuAgm = contrz(quadrAgm, quadrAgmy.conj())
+    # Cost is χ^4 χs^6
+    # For chi=[6,6], chis=[3,3], it takes about 0.06 seconds (CPU times)
+    Gamma = octu2gm(octuAgm, sy)
+    return Gamma
