a1b93febec1161ae9f3ab78df80cb44e58ef6ed7
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2024-01-11 21:58:50 +0900

    z-loop optimization implementation finished and tested

--- loop_filter/fet3dloop.py
+++ loop_filter/fet3dloop.py
@@ -33,6 +33,7 @@ with bond dimension changes χs --> χs^2 --> χ.
 _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 """
 from . import env3dloop, fet3d
+from ncon import ncon
 
 
 def init_zloopm(A, chis, chienv, epsilon):
@@ -69,11 +70,11 @@ def init_zloopm(A, chis, chienv, epsilon):
     A4x = env3dloop.swapxy(A, 1, 1)[0]
     Gammax = env3dloop.loopGamma(A4x)
     # Find initial low-rank matrix Lr and split it to get m matrix
-    sy, Lry = fet3d.init_s_gilt(Gammay, chis, chienv, epsilon,
+    my, Lry = fet3d.init_s_gilt(Gammay, chis, chienv, epsilon,
                                 init_soft=False)
-    sx, Lrx = fet3d.init_s_gilt(Gammax, chis, chienv, epsilon,
+    mx, Lrx = fet3d.init_s_gilt(Gammax, chis, chienv, epsilon,
                                 init_soft=False)
-    return sx, sy, Lrx, Lry, Gammay
+    return mx, my, Lrx, Lry, Gammay
 
 
 def init_yloopm(A, chis, chienv, epsilon):
@@ -91,6 +92,160 @@ def init_yloopm(A, chis, chienv, epsilon):
     A4z = env3dloop.swapxy(Ar, 1, 1)[0]
     Gammaz = env3dloop.loopGamma(A4z)
     # Find initial low-rank matrix Lr and split it to get m matrix
-    sz, Lrz = fet3d.init_s_gilt(Gammaz, chis, chienv, epsilon,
+    mz, Lrz = fet3d.init_s_gilt(Gammaz, chis, chienv, epsilon,
                                 init_soft=False)
-    return sz, Lrz, Gammaz
+    return mz, Lrz, Gammaz
+
+
+def optimize_zloop(A, mx, my, PsiPsi, epsilon=1e-10,
+                   iter_max=100, display=True,
+                   swapList=[False, True], checkStep=10):
+    """find mx, my matricees that maximize FET fidelity
+    z-loop is approximated here
+
+    Args:
+        A (TensorCommon): 6-leg tensor
+            It is the tensor located at
+            - (x+y+)-position for z-loop filtering after z corase graining
+            It has shape
+            - A[x, xp, y, yp, z, zp]
+        mx (TensorCommon): 2-leg tensor
+        my (TensorCommon): 2-leg tensor
+        PsiPsi (float):
+            <ψ|ψ> the norm square of the wavefunction |ψ>
+            to be optimized
+
+    Kwargs:
+        epsilon (float):
+            For pseudo-inverse of γs
+            Just for safety. Not so crucial I think
+            I set it to be the same as coarse graining one
+        iter_max (int): maximal iteration for a single direction
+        display (boolean):
+            print out the error evoluation during the optimization
+
+    Returns: new m matrices, mx, my
+
+    """
+    errList = []
+    legDic = {False: "y", True: "x"}
+    doneLegs = {False: False, True: False}
+    nleg = len(swapList)
+    for k in range(iter_max):
+        for isSwap in swapList:
+            Ap, mxp, myp = env3dloop.swapxy(A, mx, my, isSwap)
+            # This is the bottleneck of the computational cost
+            # in this loop-filtering process.
+            # The cost is χ^8
+            dbA = env3dloop.contrInLeg(Ap, Ap.conj())
+            # absorb the constant m matrix (mxp) into dbA tensor
+            dbAp, dbAgm = env3dloop.mxAssemble(dbA, mxp)
+            # using FET to optimize the other m matrix (myp)
+            mnew, err = optimize_single(dbAp, dbAgm, myp, PsiPsi, epsilon)
+            # update the other m matrix
+            if isSwap:
+                mx = mnew * 1.0
+            else:
+                my = mnew * 1.0
+            # record FET error
+            errList.append(err)
+            # check the change of FET error
+            if display and (k % checkStep == 0 or k == iter_max - 1):
+                print("Iteration {:d}.".format(k+1),
+                      "FET Error (loop) is {:.3e} (leg {:s})".format(
+                          err, legDic[isSwap])
+                      )
+                # if the change of FET error is small,
+                # the optimization for the leg is done
+                if k > 0:
+                    smallChange = (
+                        abs((errList[-1] - errList[-1 - nleg*checkStep]) / (
+                            errList[-1 - nleg * checkStep] + epsilon)
+                            ) < (0.01 * nleg * checkStep / 100)
+                    )
+                    doneLegs[isSwap] = (
+                        smallChange or (abs(errList[-1]) < epsilon)
+                    )
+        if all(doneLegs.values()):
+            break
+    return mx, my, errList
+
+
+def optimize_yloop(A, mz, PsiPsi, epsilon=1e-10,
+                   iter_max=100, display=True):
+    """find mz matrices that maximize FET fidelity
+    y-loop is approximated here
+    Similar to the above `init_zloopm` function but
+    the input tensor A located at
+        - (x+)-position for y-loop filtering after z+y corase graining
+    Also, there is only one m matrx to be inserted in the y-loop
+
+    Returns: mz
+
+    """
+    return mz
+
+
+def optimize_single(dbAp, dbAgm, mold, PsiPsi, epsilon):
+    """Optimize a single m matrix using `fet3d.updateMats`
+
+    Args:
+        dbAp (TensorCommon): 4-leg tensor
+            doubleA for P with mx absorbed
+        dbAgm (TensorCommon): 4-leg tensor
+            doubleA for γ with mx absorbed
+        mold (TensorCommon): 2-leg tensor
+            filtering matrix to be optimized
+        PsiPsi (float):
+            <ψ|ψ> the norm square of the wavefunction |ψ>
+            to be optimized
+        epsilon (float):
+            For pseudo-inverse of γs
+            Just for safety. Not so crucial I think
+
+    Returns:
+        mnew (TensorCommon): 2-leg tensor
+            the updated sy matrix
+        err (float): FET error of snew
+
+    """
+    # connstruct P and γ enviornment by absorbing `mold`
+    Pm, Gammam = env3dloop.dbA2FETenv(dbAp, dbAgm, mold)
+    # old FET error from P and γ environments
+    errOld = fet3d.cubeFidelity(mold, Pm, Gammam, PsiPsi)[1]
+    # no need for optimization if the error is already very small
+    if errOld < epsilon:
+        return mold, errOld
+    # propose a candidate s
+    mtemp = fet3d.updateMats(Pm, Gammam, epsilon=epsilon)
+    # normalized stemp (for the convex combination)
+    mtemp = mtemp / mtemp.norm()
+    # try all the convex combination of old s and new s
+    # make sure the approximation error go down
+    # (This idea is taken from Evenbly's TNR codes)
+    for p in range(11):
+        mnew = (1 - 0.1*p) * mtemp + 0.1 * p * mold
+        # connstruct P and γ enviornment by absorbing `mnew`
+        PmN, GammamN = env3dloop.dbA2FETenv(dbAp, dbAgm, mnew)
+        # calculate 1 - fidelity from new P and γ environments
+        errNew = fet3d.cubeFidelity(mnew, PmN, GammamN, PsiPsi)[1]
+        # once the FET error reduces, we update s matrix
+        if (errNew <= errOld) or (errNew < epsilon) or (p == 10):
+            # make sure the returned s matrix is normalized
+            mnew = mnew / mnew.norm()
+            break
+    return mnew, errNew
+
+
+# For absorbing mx, my, mz matrices into the intermediate main tensors
+# A1. For z-loop filtering: mx and my
+def absb_mloopz(Az, mx, my):
+    """
+    Apply mx, my matricx to
+        Az: intermediate tensor after the first z-collapse
+    """
+    Azm = ncon(
+        [Az, mx, my],
+        [[1, -2, 2, -4, -5, -6], [1, -1], [2, -3]]
+    )
+    return Azm
