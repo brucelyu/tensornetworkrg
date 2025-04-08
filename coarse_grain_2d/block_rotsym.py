1ef0277e2f9f8deefe484632e1265c9ecaa50a09
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2025-04-08 14:55:03 +0200

    construct the density matrix of the isometry

--- coarse_grain_2d/block_rotsym.py
+++ coarse_grain_2d/block_rotsym.py
@@ -9,6 +9,8 @@
 Block-tensor map with the following two lattice symmetry exploited
 - lattice reflection
 - rotational
+The 4-leg tensor A is assumed to be real.
+We have not worked out the complex-value tensor yet
 
 Tensor leg order convention is
 A[x, y, x', y']
@@ -23,17 +25,28 @@ A[x, y, x', y']
 from ncon import ncon
 
 
-def optProj(A, chi, cg_eps=1e-8):
+def densityM(A):
     """determine the isometric
 
     Args:
-        A (TODO): TODO
-        chi (TODO): TODO
+        A (TensorCommon): 4-leg tensor with real values
+        chi (int): the bond dimension χ
 
     Kwargs:
-        cg_eps (TODO): TODO
+        cg_eps (float):
+            a numerical precision control for coarse graining process
+            eigenvalues small than cg_eps will be thrown away
+            regardless of χ
 
-    Returns: TODO
+    Returns:
+        p (TensorCommon): 3-leg tensor
+        g (array): SWAP sign matrix
 
     """
-    pass
+    rho = ncon([A, A.conjugate(), A.conjugate(), A],
+               [[-1, 2, 1, 5], [-2, 4, 3, 5],
+                [-3, 2, 1, 6], [-4, 4, 3, 6]]
+               )
+    # This matrix has the symmetry under the SWAP operation:
+    #   rho[i,j,m,n] = rho[j,i,n,m]
+    return rho
