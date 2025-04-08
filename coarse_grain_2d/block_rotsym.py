03c9a2e13957a4dd61deec7290efb8b73b1d3f19
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2025-04-08 23:13:24 +0200

    find isometry

--- coarse_grain_2d/block_rotsym.py
+++ coarse_grain_2d/block_rotsym.py
@@ -22,14 +22,34 @@ A[x, y, x', y']
 
 """
 
+import numpy as np
 from ncon import ncon
 
 
 def densityM(A):
-    """determine the isometric
+    """Construct the density matrix for the isometry
 
     Args:
         A (TensorCommon): 4-leg tensor with real values
+
+    Returns:
+        rho (TensorCommon): 4-leg density matrix
+
+    """
+    rho = ncon([A, A.conjugate(), A.conjugate(), A],
+               [[-1, 2, 1, 5], [-2, 4, 3, 5],
+                [-3, 2, 1, 6], [-4, 4, 3, 6]]
+               )
+    # This matrix has the symmetry under the SWAP operation:
+    #   rho[i,j,m,n] = rho[j,i,n,m]
+    return rho
+
+
+def findProj(rho, chi, cg_eps=1e-8):
+    """determine the isometry p
+
+    Args:
+        rho (TensorCommon): density matrix for isometry
         chi (int): the bond dimension χ
 
     Kwargs:
@@ -40,13 +60,44 @@ def densityM(A):
 
     Returns:
         p (TensorCommon): 3-leg tensor
-        g (array): SWAP sign matrix
+        g (array): SWAP sign
+        err (float): error for truncation
+        eigv (TensorCommon): eigenvalues
 
     """
-    rho = ncon([A, A.conjugate(), A.conjugate(), A],
-               [[-1, 2, 1, 5], [-2, 4, 3, 5],
-                [-3, 2, 1, 6], [-4, 4, 3, 6]]
-               )
-    # This matrix has the symmetry under the SWAP operation:
-    #   rho[i,j,m,n] = rho[j,i,n,m]
-    return rho
+    # simontaneously diagonalize rho and the SWAP matrix
+    # since SWAP @ rho = rho @ SWAP.
+    # SWAP @ rho[ij mn] = rho[ji mn]
+    # Eigenvalues are the multiplication of the two
+    # (The second pair is [3, 2] instead of [2, 3] due to SWAP)
+    eigv, p, err = rho.eig(
+        [0, 1], [3, 2],
+        chis=[i+1 for i in range(chi)], eps=cg_eps,
+        trunc_err_func=trunc_err_func,
+        return_rel_err=True
+    )
+    # calculate the SWAP eigenvalues
+    g = np.sign(eigv)
+    return p, g, err, eigv.abs()
+
+
+def block4ten(A, p):
+    """contraction of a 2x2 block of tensors
+
+    Args:
+        A (TODO): TODO
+        p (TODO): TODO
+
+    Returns: TODO
+
+    """
+    return Ap
+
+
+def trunc_err_func(eigv, chi):
+    """
+    No need to take square since we work with M M'
+    whose eigenvalues are themself square of singular values of M
+    """
+    res = np.sum(np.abs(eigv[chi:])) / np.sum(np.abs(eigv))
+    return res
