3fb6d3c35fd55fec121850a33f758798a8c63ef3
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2025-04-09 18:13:16 +0200

    check symmetries

--- coarse_grain_2d/block_rotsym.py
+++ coarse_grain_2d/block_rotsym.py
@@ -45,7 +45,7 @@ def densityM(A):
     return rho
 
 
-def findProj(rho, chi, cg_eps=1e-8):
+def findProj(A, chi, cg_eps=1e-8):
     """determine the isometry p
 
     Args:
@@ -72,14 +72,18 @@ def findProj(rho, chi, cg_eps=1e-8):
     # SWAP @ rho[ij mn] = rho[ji mn]
     # Eigenvalues are the multiplication of the two
     # (The second pair is [3, 2] instead of [2, 3] due to SWAP)
+    rho = densityM(A)
     eigv, p, err = rho.eig(
         [0, 1], [3, 2],
         chis=[i+1 for i in range(chi)], eps=cg_eps,
         trunc_err_func=trunc_err_func,
         return_rel_err=True
     )
+    # all eigenvectors and eigenvalues are real
+    eigv = eigv.real()
+    p = p.real()
     # calculate the SWAP eigenvalues
-    g = np.sign(eigv)
+    g = eigv.sign()
     return p, g, err, eigv.abs()
 
 
@@ -112,5 +116,103 @@ def trunc_err_func(eigv, chi):
     """
     res = np.sum(np.abs(eigv[chi:])) / np.sum(np.abs(eigv))
     return res
+
+
+# Functions related to the symmetry of the tensor
+def reflect(A, axis, g=None):
+    assert axis in ["x", "y"]
+    if g is None:
+        if axis == "x":
+            Arefl = A.transpose([2, 1, 0, 3])
+        elif axis == "y":
+            Arefl = A.transpose([0, 3, 2, 1])
+    else:
+        # convert array g to a diagonal matrix with dirs=[1, 1]
+        gM = g.diag()               # dirs=[1, -1] by default
+        gM = gM.flip_dir(1)
+        if axis == "x":
+            Arefl = ncon(
+                [A.transpose([2, 1, 0, 3]).conj(),
+                 gM, gM.conj(), gM, gM.conj()],
+                [[1, 2, 3, 4], [1, -1], [2, -2], [3, -3], [4, -4]])
+        elif axis == "y":
+            Arefl = ncon(
+                [A.transpose([0, 3, 2, 1]).conj(),
+                 gM, gM.conj(), gM, gM.conj()],
+                [[1, 2, 3, 4], [1, -1], [2, -2], [3, -3], [4, -4]])
+    return Arefl
+
+
+def rotate(A, g=None):
+    if g is None:
+        # conjugate here is just for Z2-symmetric tensors dirs
+        Arot = A.transpose([3, 0, 1, 2]).conj()
+    else:
+        # convert array g to a diagonal matrix with dirs=[1, 1]
+        gM = g.diag()               # dirs=[1, -1] by default
+        gM = gM.flip_dir(1)
+        Arot = ncon(
+            [A.transpose([3, 0, 1, 2]), gM, gM.conj(), gM, gM.conj()],
+            [[1, 2, 3, 4], [1, -1], [2, -2], [3, -3], [4, -4]])
+    return Arot
+
+
+def isReflSym(A, g=None):
+    """Check the reflection symmetry of the tensor
+
+    Args:
+        A (TensorCommon): 4-leg tensor with real values
+        g (array): SWAP sign
+
+    Returns:
+        isX, isY (boolean)
+
+    """
+    Areflx = reflect(A, "x", g)
+    Arefly = reflect(A, "y", g)
+    isX = Areflx.allclose(A)
+    isY = Arefly.allclose(A)
+    return isX, isY
+
+
+def isRotSym(A, g=None):
+    """Check the rotation symmetry
+
+    Args:
+        A (TensorCommon): 4-leg tensor with real values
+
+    Kwargs:
+        g (array): SWAP sign
+
+    Returns: isRot
+
+    """
+    Arot = rotate(A, g)
+    isRot = Arot.allclose(A)
+    return isRot
+
+
+def makeReflSym(A, g=None):
+    """symmetrize the tensor A w.r.t reflection
+
+    Args:
+        A (TensorCommon): 4-leg tensor with real values
+
+    Returns:
+        Asym (TensorCommon): symmetrized tensor
+
+    """
+    Ax = 0.5 * (A + reflect(A, "x", g))
+    Axy = 0.5 * (Ax + reflect(Ax, "y", g))
+    return Axy
+
+
+def makeRotSym(A, g=None):
+    A90 = rotate(A, g)
+    A180 = rotate(A90, g)
+    A270 = rotate(A180, g)
+    Asym = 0.25 * (A + A90 + A180 + A270)
+    return Asym
+
 
 # end of file
