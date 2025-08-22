81229f3798794cba6d5dd41542b918087e9062f0
Xinliang(Bruce) Lyu <lyu@issp.u-tokyo.ac.jp>
2025-08-22 14:32:27 +0200

    build env tensors for optimization of relfsym-EF

--- loop_filter/env2d_reflsym.py
+++ loop_filter/env2d_reflsym.py
@@ -55,4 +55,51 @@ def build_Upsilon0(A, bond="x"):
     Upsilon0 = env2d_rotsym.dbA2Upsilon0(dbA)
     return Upsilon0
 
+
+def build_UpsilonQ(A, sx, sy, bond="x"):
+    """Build Upsilon and Q
+    For the optimization of the filtering matrices
+
+    Args:
+        A (TensorCommon): 6-leg tensor
+        sx,sy (TensorCommon): 2-leg tensor
+            the filtering matrices
+
+    Kwargs:
+        bond (str): choose in ["x", "y"]
+
+    Returns:
+        Upsilon (TensorCommon): 4-leg tensor
+        Q (TensorCommon): 2-leg tensor
+
+    """
+    assert bond in ["x", "y"]
+    # The prototype is for x bond
+    if bond == "x":
+        Arel = A * 1.0
+        sxrel = sx * 1.0
+        syrel = sy * 1.0
+    else:
+        Arel = A.transpose([1, 0, 3, 2])
+        sxrel = sy * 1.0
+        syrel = sx * 1.0
+
+    dbA = env2d_rotsym.contr2A(Arel)
+    # these two are for contructing tensor Q
+    dbAsy = ncon([dbA, syrel.conj()],
+                 [[-1, -2, -3, 1], [1, -4]])
+    dbAsyx = ncon([dbAsy, sxrel.conj()],
+                  [[-1, 1, -3, -4], [1, -2]])
+    # these two are for contructing tensor Upsilon
+    dbAsyy = ncon([dbAsy, syrel],
+                  [[-1, -2, 1, -4], [1, -3]])
+    dbA4s = ncon([dbAsyy, sxrel, sxrel.conj()],
+                 [[1, 2, -3, -4], [1, -1], [2, -2]])
+    # construct Upsilon and Q
+    Upsilon = env2d_rotsym.contr4dbA(dbAsyy, dbA4s)
+    Qten = env2d_rotsym.contr4dbA(dbAsy, dbAsyx)
+    Q = ncon([Qten], [[-1, -2, 1, 1]])
+    return Upsilon, Q
+
+
 # end of file
--- loop_filter/fet2d_reflsym.py
+++ loop_filter/fet2d_reflsym.py
@@ -25,3 +25,42 @@ from .. import u1ten
 from . import env2d_rotsym, env2d_reflsym
 from ncon import ncon
 
+
+# I. For the initialization of the filtering matriices sx and sy
+def init_s(A, chis, chienv, epsilon, epsilon_inv=1e-10,
+           bond="x"):
+    """Initialization of the filtering matrix s
+
+    Args:
+        A (TensorCommon): 4-leg tensor
+        chis (int): χs in the filtering
+        chienv (int): χenv for taking the pinv of Upsilon0
+        epsilon (float): for the pinv
+
+    Returns:
+        s (TensorCommon): the filtering matrix
+        Lr (TensorCommon): the low-rank matrix, Lr = s @ s*
+        Upsilon0 (TensorCommon): bone environment tensor
+        dbA (TensorCommon): contraction of two copies of A:
+            useful for later optimization of s
+
+    """
+    Upsilon0 = env2d_reflsym(A, bond=bond)
+    # determine the low-rank matrix (like Gilt without recursion)
+    # Step 1. Take the Moore-Penrose inverse of the Upsilon tensor
+    Upsilon0_pinv = u1ten.pinv(
+        Upsilon0, [0, 1], [2, 3],
+        eps_mach=epsilon_inv, chiCut=chienv
+    )
+    # Step 2. Obtain the low-rank matrix Lr
+    Lr = ncon([Upsilon0_pinv, Upsilon0], [[-1, -2, 1, 2], [1, 2, 3, 3]])
+    # Step 3. Split Lr to get s: Lr = s @ s*
+    s = Lr.split([0], [1], chis=[k+1 for k in range(chis)],
+                 eps=epsilon)[0]
+    # make the direction of s as [-1, 1]
+    s = s.flip_dir(1)
+    return s, Lr, Upsilon0
+
+
+
+# end of file
